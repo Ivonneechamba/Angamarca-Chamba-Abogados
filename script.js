36af87a59e9ccbed962d11e18d72b24ec341b899
Ivonneechamba <elizabethchamba2012@gmail.com>
2026-06-11 22:13:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function enviarWhatsApp(){
-    let nombre = document.getElementById('nombre').value;
-    let telefono = document.getElementById('telefono').value;
-    let mensaje = document.getElementById('mensaje').value;
-    let modalidad = document.getElementById('modalidad').value;
-
-    let url = "https://wa.me/593985220503?text="
-    + "Nombre: " + nombre + "%0A"
-    + "Teléfono: " + telefono + "%0A"
-    + "Modalidad: " + modalidad + "%0A"
-    + "Consulta: " + mensaje;
-
-    window.open(url, '_blank');
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href"))
+            .scrollIntoView({ behavior: "smooth" });
+    });
+});
